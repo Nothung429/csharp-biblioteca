035ec73f5946ba91feceadd23cf155b078c31f2c
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-20 15:29:59 +0200

    add user verification and first search

--- Program.cs
+++ Program.cs
@@ -19,35 +19,72 @@
 //L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
 //Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
 
-Book myBook = new Book(12345678, 1200,"Il signore degli Anelli", 1954, "J. R. R. Tolkien", "fantasy", true);
+//creazione utente 
+User myUser = new User("Mario", "Rossi", "mariorossi@gmail.com", "prova123", 333777888);
 
-Console.WriteLine($"\nL'ISBN del libro è: {myBook.GetIsbn()}");
-Console.WriteLine($"Il numero di pagine del libro è: {myBook.GetPageNumber()}");
-Console.WriteLine($"Il titolo del libro è: {myBook.title}");
-Console.WriteLine($"L'anno di uscita del libro è: {myBook.year}");
-Console.WriteLine($"L'autore del libro è: {myBook.author}");
-Console.WriteLine($"La categoria del libro è: {myBook.category}");
-Console.WriteLine($"La disponibilità del libro è: {myBook.status} \n");
+//creazione prodotti
+Book myBook = new Book(12345678, 1200, "il signore degli anelli", 1954, "J. R. R. Tolkien", "fantasy", true);
+Film myFilm = new Film(87654321, 200, "il signore degli anelli: la compagnia dell'anello", 2001, "J. R. R. Tolkien", "fantasy", true);
 
-Film myFilm = new Film(87654321, 200,"Il signore degli Anelli: la compagnia dell'anallo", 2001, "J. R. R. Tolkien", "fantasy", true);
+Console.WriteLine("Esegui l'accesso");
+bool verifyUserEmail = myUser.GetEmail().Contains(Console.ReadLine());
+bool verifyUserPassword = myUser.GetPassword().Contains(Console.ReadLine());
+if (verifyUserEmail == true && verifyUserPassword == true)
+{
+    //ricerca
+    Console.WriteLine("Cerca un libro");
+    bool searchBooks = myBook.title.Contains(Console.ReadLine());
+    if (searchBooks == true)
+    {
+        Console.WriteLine($"Il libro esiste in libreria? {myBook.title} \n");
+    }
+    else
+    {
+        Console.WriteLine("Ci dispiace non abbiamo questo titolo \n");
+    }
 
-Console.WriteLine($"\nIl codice del film è: {myFilm.GetCode()}");
-Console.WriteLine($"La durata del film è: {myFilm.GetDuration()} minuti");
-Console.WriteLine($"Il titolo del film è: {myFilm.title}");
-Console.WriteLine($"L'anno di uscita del film è: {myFilm.year}");
-Console.WriteLine($"L'autore del film è: {myFilm.author}");
-Console.WriteLine($"La categoria del film è: {myFilm.category}");
-Console.WriteLine($"La disponibilità del film è: {myFilm.status} \n");
+    Console.WriteLine("Cerca un film");
+    bool searchFilms = myFilm.title.Contains(Console.ReadLine());
+    if (searchFilms == true)
+    {
+        Console.WriteLine($"Il film esiste in libreria? {myFilm.title} \n");
+    }
+    else
+    {
+        Console.WriteLine("Ci dispiace non abbiamo questo titolo \n");
+    }
+}
+else
+{
+    Console.WriteLine("Registrati merda");
+}
 
+//Console.WriteLine($"\nL'ISBN del libro è: {myBook.GetIsbn()}");
+//Console.WriteLine($"Il numero di pagine del libro è: {myBook.GetPageNumber()}");
+//Console.WriteLine($"Il titolo del libro è: {myBook.title}");
+//Console.WriteLine($"L'anno di uscita del libro è: {myBook.year}");
+//Console.WriteLine($"L'autore del libro è: {myBook.author}");
+//Console.WriteLine($"La categoria del libro è: {myBook.category}");
+//Console.WriteLine($"La disponibilità del libro è: {myBook.status} \n");
+
+//Console.WriteLine($"\nIl codice del film è: {myFilm.GetCode()}");
+//Console.WriteLine($"La durata del film è: {myFilm.GetDuration()} minuti");
+//Console.WriteLine($"Il titolo del film è: {myFilm.title}");
+//Console.WriteLine($"L'anno di uscita del film è: {myFilm.year}");
+//Console.WriteLine($"L'autore del film è: {myFilm.author}");
+//Console.WriteLine($"La categoria del film è: {myFilm.category}");
+//Console.WriteLine($"La disponibilità del film è: {myFilm.status} \n");
+
+//funzioni
 public class User
 {
     private string name;
     private string surname;
     private string email;
     private string password;
-    private string phone;
+    private int phone;
 
-    public User(string name, string surname, string email, string password, string phone)
+    public User(string name, string surname, string email, string password, int phone)
     {
         this.name = name;
         this.surname = surname;
@@ -72,15 +109,12 @@ public class User
     {
         return this.password;
     }
-    public string GetPhone()
+    public int GetPhone()
     {
         return this.phone;
     }
 }
-
 //--------------------------------------------------------------------------//
-
-
 public class Product
 {
     protected internal int year;
@@ -98,12 +132,7 @@ public class Product
         this.status = status;
     }
 }
-
 //--------------------------------------------------------------------------//
-
-
-
-
 public class Book : Product
 {
     private int ISBN;
@@ -124,7 +153,6 @@ public class Book : Product
         return this.pageNumber;
     }
 }
-
 //--------------------------------------------------------------------------//
 public class Film : Product
 {
