3ecce37c73498276dac0c017f832a416c882c986
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-20 14:33:58 +0200

    add first data

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.ConstrainedExecution;
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
 //Si vuole progettare un sistema per la gestione di una biblioteca. Gli utenti si possono registrare al sistema, fornendo:
 //    cognome,
@@ -20,5 +17,92 @@ Console.WriteLine("Hello, World!");
 //    un autore (Nome, Cognome).
 
 //Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
+
 //L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
 //Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+
+public class User
+{
+    private string name;
+    private string surname;
+    private string email;
+    private string password;
+    private string phone;
+
+    public User(string name, string surname, string email, string password, string phone)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public string GetName()
+    {
+        return this.name;
+    }
+    public string GetSurname()
+    {
+        return this.surname;
+    }
+    public string GetEmail()
+    {
+        return this.email;
+    }
+    public string GetPassword()
+    {
+        return this.password;
+    }
+    public string GetPhone()
+    {
+        return this.phone;
+    }
+}
+
+public class Product
+{
+    protected internal int year;
+    protected internal string title;
+    protected internal string author;
+    protected internal string category;
+    protected internal bool status;
+
+    public Product(int year, string title, string author, string category, bool status)
+    {
+        this.year = year;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.status = status;
+    }
+}
+
+public class Book : Product
+{
+    private int ISBN;
+
+    public Book(int ISBN, int year, string title, string author, string category, bool status) : base(year, title, author, category, status)
+    {
+        this.ISBN = ISBN;
+    }
+
+    public int GetIsbn()
+    {
+        return this.ISBN;
+    }
+}
+
+public class Film : Product
+{
+    private int code;
+
+    public Film(int code, int year, string title, string author, string category, bool status) : base(year, title, author, category, status)
+    {
+        this.code = code;
+    }
+    public int GetCode()
+    {
+        return this.code;
+    }
+}
